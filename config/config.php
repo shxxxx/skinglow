a90c0a460f50f5353d92b36481445f4e80cc1b99
shxx <57565241+shxxxx@users.noreply.github.com>
2022-06-28 16:07:23 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_URL', 'http://localhost/skinglow/'); //add your localhost link
 define('DB_HOST', 'localhost');
-define('DB_USER', 'Shurooq');//add your name
-define('DB_PASS', 'password@123');
+define('DB_USER', '');//add your name
+define('DB_PASS', '');
 define('DB_NAME', 'skinglow');
 ?>
